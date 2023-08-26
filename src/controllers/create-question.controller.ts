8f10ad52e2f5c174aefecd75623dd9ec37008b86
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-08-26 13:16:17 -0300

    📋 feat: Controller de listagem de perguntas

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,10 +1,10 @@
+import { CurrentUser } from "@/auth/current-user-decorator";
+import { JwtAuthGuard } from "@/auth/jwt-auth.guard";
+import { UserPayload } from "@/auth/jwt.strategy";
+import { ZodValidationPipe } from "@/pipes/zod-validation.pipe";
+import { PrismaService } from "@/prisma/prisma.service";
+import { formatStringToId } from "@/utils/format-string-to-id";
 import { Body, Controller, Post, UseGuards } from "@nestjs/common";
-import { CurrentUser } from "src/auth/current-user-decorator";
-import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
-import { UserPayload } from "src/auth/jwt.strategy";
-import { ZodValidationPipe } from "src/pipes/zod-validation.pipe";
-import { PrismaService } from "src/prisma/prisma.service";
-import { formatStringToId } from "src/utils/format-string-to-id";
 import { z } from "zod";
 
 const createQuestionBodySchema = z.object({
