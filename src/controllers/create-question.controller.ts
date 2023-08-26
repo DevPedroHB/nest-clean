394fd06ea145b3b330da9252d7ad262b7b835ad1
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-08-26 12:16:49 -0300

    🛡️ feat: Protegendo rotas com Guards

---
+++ src/controllers/create-question.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Post, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+
+// const authenticateBodySchema = z.object({
+//   email: z.string().email(),
+//   password: z.string(),
+// });
+
+// type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+@Controller("/questions")
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  @Post()
+  // @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle() {}
+}
