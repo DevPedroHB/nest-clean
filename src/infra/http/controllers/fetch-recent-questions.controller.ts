1ee94f98034315d7d8d34a8becb33183f92eeaf2
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-09-02 04:32:08 -0300

    🔒 feat: Rotas privadas por padrão

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -1,14 +1,7 @@
 import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions";
 import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation.pipe";
-import {
-  BadRequestException,
-  Controller,
-  Get,
-  Query,
-  UseGuards,
-} from "@nestjs/common";
+import { BadRequestException, Controller, Get, Query } from "@nestjs/common";
 import { z } from "zod";
-import { JwtAuthGuard } from "../../auth/jwt-auth.guard";
 import { QuestionPresenter } from "../presenters/question-presenter";
 
 const pageQueryParamSchema = z
@@ -23,7 +16,6 @@ const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema);
 type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>;
 
 @Controller("/questions")
-@UseGuards(JwtAuthGuard)
 export class FetchRecentQuestionsController {
   constructor(private fetchRecentQuestions: FetchRecentQuestionsUseCase) {}
 
