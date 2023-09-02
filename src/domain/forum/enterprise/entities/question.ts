e50805131b872b83d85b84359ea293d82bc34b70
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-09-02 01:22:16 -0300

    📦 feat: Criação do schema do Prisma

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -8,7 +8,7 @@ import { Slug } from "./value-objects/slug";
 
 export interface IQuestion {
   authorId: UniqueEntityID;
-  bestAnswerId?: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID | null;
   title: string;
   content: string;
   slug: Slug;
@@ -26,7 +26,7 @@ export class Question extends AggregateRoot<IQuestion> {
     return this.props.bestAnswerId;
   }
 
-  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
     if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
       this.addDomainEvent(
         new QuestionBestAnswerChosenEvent(this, bestAnswerId)
