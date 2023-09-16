781cbc37f23c041ad4243cb1f6bd9a235e6430b2
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-09-16 10:27:50 -0300

    🧪 feat: Utilizando Factories nos Testes E2E

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,7 +3,10 @@ import {
   IQuestion,
   Question,
 } from "@/domain/forum/enterprise/entities/question";
+import { PrismaQuestionMapper } from "@/infra/database/prisma/mappers/prisma-question-mapper";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
 import { faker } from "@faker-js/faker";
+import { Injectable } from "@nestjs/common";
 
 export function makeQuestion(
   override: Partial<IQuestion> = {},
@@ -20,4 +23,19 @@ export function makeQuestion(
   );
 
   return question;
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(data: Partial<IQuestion> = {}) {
+    const question = makeQuestion(data);
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    });
+
+    return question;
+  }
 }
