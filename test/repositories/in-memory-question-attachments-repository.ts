f3472aca5fb67545b71006da61260819e63b87e5
DevPedroHB <pedroh.bergamo20@gmail.com>
2023-09-23 05:14:52 -0300

    📎 feat: Perguntas com Anexos

--- test/repositories/in-memory-question-attachments-repository.ts
+++ test/repositories/in-memory-question-attachments-repository.ts
@@ -1,8 +1,10 @@
+/* eslint-disable indent */
+
 import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
 import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
 
 export class InMemoryQuestionAttachmentsRepository
-implements QuestionAttachmentsRepository
+  implements QuestionAttachmentsRepository
 {
   public items: QuestionAttachment[] = [];
 
@@ -12,6 +14,18 @@ implements QuestionAttachmentsRepository
     );
 
     return questionAttachments;
+  }
+
+  async createMany(attachments: QuestionAttachment[]) {
+    this.items.push(...attachments);
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]) {
+    const questionAttachments = this.items.filter((item) => {
+      return !attachments.some((attachment) => attachment.equals(item));
+    });
+
+    this.items = questionAttachments;
   }
 
   async deleteManyByQuestionId(questionId: string) {
